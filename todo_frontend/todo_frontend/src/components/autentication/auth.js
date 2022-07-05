aa5de37459447dbcb604fb060e8c71c4cb6a160e
DESKTOP-93HS66K\nkdel <nkdelbazz113@gmail.com>
2022-07-06 00:12:18 +0200

    [auth.js]  sistemazione completa del form di validazione con gestione errori di battitura e abilitazione tasto submit

--- todo_frontend/todo_frontend/src/components/autentication/auth.js
+++ todo_frontend/todo_frontend/src/components/autentication/auth.js
@@ -31,9 +31,41 @@ DA FARE : onChange={handlePasswordChange}
     props.func(_action)
   }
 
+  const [password, setPassword] = useState('')
+  const [errorValPassword,setValidationPassword] = useState(true)
+
+  const [email, setEmail] = useState('')
+  const [errorValEmail,setValidationEmail] = useState(true)
+
+  const [invio, setInvio] = useState(true)
+
+  const validation_email = email.length >= 10 && email.includes('@') && email.includes('.');
+  var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  const validation_password = format.test(password) ;
+
   useEffect(() => {
-    alert("You have do this times");
-  });
+    const timeOutId = setTimeout(() => {
+      if( validation_email && validation_password)
+      {
+        console.log("input data ok")
+        setInvio(false)
+      }
+      else
+      {
+        console.log("input data non ok")
+        setInvio(true)
+      }
+    }, 500);
+    return () => clearTimeout(timeOutId);
+  }, [email, password]) // Only re-run the effect if name or email change
+
+  function getControlEmail(){
+    setValidationEmail(validation_email)
+  }
+
+  function getControlPassword(){
+    setValidationPassword(validation_email)
+  }
 
 
   switch (_action) {
@@ -45,16 +77,40 @@ DA FARE : onChange={handlePasswordChange}
               <div style={{ display: "flex",lineHeight:"50px"}} className={styles.form_section}>
                 <div style={{ width:"400px"}}>LOGIN</div>
               </div>
-              <div style={{ display: "flex",lineHeight:"50px"}} className={styles.form_section}>
+
+              <div style={{ display: "flex",height:"50px",verticalAlign:"middle"}} className={styles.form_section}>
                 <div style={{ width:"250px",paddingLeft:"30px"}}>Email</div>
-                <div style={{ flexGrow: "1",paddingRight:"30px"}}><input style={{ width: "250px"}}></input></div>
+                <div style={{ flexGrow: "1",paddingRight:"30px"}}>
+                  <input 
+                        onKeyUp  = { e => setEmail(e.target.value) } 
+                        onBlur={e => getControlEmail()}
+                        style={{ width: "250px",backgroundColor: (!errorValEmail && "rgba(103, 11, 11, 0.208)")}}>
+                  </input>
+                   {!errorValEmail &&
+                    (
+                      <span style={{ width:"250px",alignItems:"left",color:"rgba(103, 11, 11, 0.608)"}}>email contains '@' and '.' </span>
+                    ) 
+                    }
+                </div>
               </div>
-              <div style={{ display: "flex",lineHeight:"50px"}} className={styles.form_section}>
+
+              <div style={{ display: "flex",height:"50px",verticalAlign:"middle"}} className={styles.form_section}>
                 <div style={{ width:"250px",paddingLeft:"30px"}}>Password</div>
-                <div style={{ flexGrow: "1",paddingRight:"30px"}}><input style={{ width: "250px"}}></input></div>
+                <div style={{ flexGrow: "1",paddingRight:"30px"}}>
+                  <input 
+                        onKeyUp  = { e => setPassword(e.target.value) }
+                        onBlur={e => getControlPassword()}
+                        style={{ width: "250px",backgroundColor: (!errorValPassword && "rgba(103, 11, 11, 0.208)")}}>
+                  </input>
+                  {!errorValPassword &&
+                    (
+                      <span style={{ width:"250px",alignItems:"left",color:"rgba(103, 11, 11, 0.608)"}}>'Password contains special chars' </span>
+                    ) 
+                    }
+                  </div>
               </div>
-              <div style={{ display: "flex",lineHeight:"50px"}} className={styles.form_section}>
-                <div onClick={() => richiesta("none")} style={{ width:"400px"}} className={styles.submit}>submit</div>
+              <div style={{ display: "flex",lineHeight:"50px",paddingLeft:"150px",paddingBottom:"5px"}} className={styles.form_section}>
+                <button id={styles.button_submit} disabled={invio} onClick={() => richiesta("none")} style={{ width:"100px",backgroundColor:""}} className={styles.submit}>submit</button>
               </div>
               </div>
             </div>
